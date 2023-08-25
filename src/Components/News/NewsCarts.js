f0f26142823bbdbf18b981958d830a147407a8c9
Rup medhi <rupmmedhi@gmail.com>
2023-08-25 10:43:01 +0530

    add pagination using usestate hook

--- src/Components/News/NewsCarts.js
+++ src/Components/News/NewsCarts.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchNews } from '../Redux/Slices/NewsSlices';
 import './NewsCarts.css'; // Import your CSS file for styling
@@ -6,6 +6,8 @@ import './NewsCarts.css'; // Import your CSS file for styling
 export default function NewsCarts() {
     const { data, isLoading, error } = useSelector((state) => state.NewsSlices);
     const dispatch = useDispatch();
+    const itemsPerPage = 6; // Number of articles to display per page
+    const [currentPage, setCurrentPage] = useState(1);
 
     const handleFetchNews = (category) => {
         dispatch(fetchNews({ category }));
@@ -16,7 +18,11 @@ export default function NewsCarts() {
         console.log(data.articles.length)
     }, []);
 
-    const displayedNews = data && data.articles ? data.articles.slice(0, 6) : [];
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    const displayedNews = data && data.articles ? data.articles.slice(startIndex, endIndex) : [];
+
 
     return (
         <div className='news_container'>
@@ -35,6 +41,22 @@ export default function NewsCarts() {
             ) : (
                 <span>no data</span>
             )}
+               {/* Pagination Buttons */}
+               <div className="pagination-buttons">
+                <button
+                    onClick={() => setCurrentPage(currentPage - 1)}
+                    disabled={currentPage === 1}
+                >
+                    Previous
+                </button>
+                <button
+                    onClick={() => setCurrentPage(currentPage + 1)}
+                    disabled={endIndex >= data.articles.length}
+                >
+                    Next
+                </button>
+            </div>
         </div>
+      
     );
 }
